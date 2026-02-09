32ebc38254242a42411576ab611748032b107725
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-02-09 16:49:46 +0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-let filter = document.getElementById("filter");
+let filter = document.getElementById("categoryFilter");
 let cards = document.querySelectorAll(".card");
 
 filter.addEventListener("change", function () {
   let value = filter.value;
 
-  cards.forEach(card => {
-    if (value === "all" || card.dataset.category === value) {
+  cards.forEach(function (card) {
+    if (value === "all") {
       card.style.display = "block";
-    } else {
+    } 
+    else if (card.classList.contains(value)) {
+      card.style.display = "block";
+    } 
+    else {
       card.style.display = "none";
     }
   });
 });
-
